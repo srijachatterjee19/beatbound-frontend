0da4c8ecc7eef75ac6271463df9b1dd6125aac9c
srijachatterjee19 <srijachatterjee9218@gmail.com>
2026-04-04 22:25:06 +0530

    Add mock data

---
+++ src/data/mockData.js
@@ -0,0 +1,23 @@
+export const MUSIC_DATA = [
+    {
+      id: 1,
+      name: 'The Beatles',
+      genre: 'Rock',
+      album: 'Abbey Road',
+      songs: ['Come Together', 'Something', 'Oh! Darling']
+    },
+    {
+      id: 2,
+      name: 'Daft Punk',
+      genre: 'Electronic',
+      album: 'Discovery',
+      songs: ['One More Time', 'Digital Love', 'Harder, Better, Faster, Stronger']
+    },
+    {
+      id: 3,
+      name: 'Kendrick Lamar',
+      genre: 'Hip Hop',
+      album: 'DAMN.',
+      songs: ['HUMBLE.', 'DNA.', 'LOYALTY.']
+    }
+  ];
